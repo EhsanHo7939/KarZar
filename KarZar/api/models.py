dead89ad0834d837d67b9694829dff926e5bef83
Ehsan Ho <EhsanHo7939@gmail.com>
2021-11-09 12:42:47 +0330

    Create Question Model

--- KarZar/api/models.py
+++ KarZar/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Question(models.Model):
+    Q_Body = models.TextField()
+    
+    Option_1 = models.TextField()
+    Option_2 = models.TextField()
+    Option_3 = models.TextField()
+    Option_4 = models.TextField()
+
+    Extra_Option = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.Q_Body[:30]
+
+
+    class Meta:
+        ordering = ['-updated']
--- KarZar/api/views.py
+++ KarZar/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/test1/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns Test1'
+        },
+        {
+            'Endpoint': '/test2/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns Test2'
+        },
+        {
+            'Endpoint': '/test3/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns Test3'
+        },
+        {
+            'Endpoint': '/test4/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns Test4'
+        },
+        
+    ]
+
+    return Response(routes)
