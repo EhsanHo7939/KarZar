531120e5756eba5bb6f40b2f6d544b38b20c2ae5
Sadra Galavani <sadra.galavani78@gmail.com>
2021-11-18 17:05:32 +0330

    question screen done

--- lib/custom_widgets/question_holder_bubble.dart
+++ lib/custom_widgets/question_holder_bubble.dart
@@ -12,13 +12,15 @@ class _QuestionHolderBubbleState extends State<QuestionHolderBubble> {
   bool? isActive2 = false;
   bool? isActive3 = false;
   bool? isActive4 = false;
+  TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 600,
       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
       decoration: const BoxDecoration(
-        color: Colors.white70,
+        color: Colors.white,
         borderRadius: BorderRadius.all(Radius.circular(25)),
         boxShadow: [
           BoxShadow(offset: Offset(0, 0), blurRadius: 25, color: Colors.black26)
@@ -28,103 +30,171 @@ class _QuestionHolderBubbleState extends State<QuestionHolderBubble> {
         textDirection: TextDirection.rtl,
         child: Column(
           children: <Widget>[
-            Row(
-              children: const <Widget>[
-                Icon(Icons.image),
-                Text('question?'),
-              ],
-            ),
-            Card(
-              shape: const RoundedRectangleBorder(
-                side: BorderSide(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-              ),
-              child: ListTile(
-                title: Text(
-                  'نظر یک اینگونه میباشد',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyle(
-                    color: isActive1! ? Colors.black : Colors.black87,
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 150,
+                    width: 150,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(25)),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          "https://cdn.smarttiz.com/Image/News/khoy-cityy_980814152840087.jpg",
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                onTap: () {
-                  setState(() => isActive1 = !isActive1!);
+                  const Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 24),
+                      child: Directionality(
+                        textDirection: TextDirection.rtl,
+                        child: Text(
+                          'سوال مربوط به نظر سنجی اینجا قرار خواهد گرفت.',
+                          softWrap: true,
+                          maxLines: 5,
+                          overflow: TextOverflow.visible,
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            QuestionMaker(
+              isActive: isActive1,
+              textMsg: 'نظر یک اینگونه میباشد',
+              side: isActive1!
+                  ? const BorderSide(color: Colors.cyan, width: 1)
+                  : const BorderSide(color: Colors.grey, width: 1),
+              onTap: () {
+                setState(() {
+                  controller.clear();
+                  isActive1 = true;
                   isActive2 = false;
                   isActive3 = false;
                   isActive4 = false;
-                },
-              ),
-              color: isActive1! ? Colors.blueAccent : Colors.white,
+                });
+              },
             ),
-            Card(
-              shape: const RoundedRectangleBorder(
-                side: BorderSide(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-              ),
-              child: ListTile(
-                title: Text(
-                  'نظر دو اینگونه میباشد',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyle(
-                    color: isActive2! ? Colors.black : Colors.black87,
-                  ),
-                ),
-                onTap: () {
-                  setState(() => isActive2 = !isActive2!);
+            QuestionMaker(
+              isActive: isActive2,
+              side: isActive2!
+                  ? const BorderSide(color: Colors.cyan, width: 1)
+                  : const BorderSide(color: Colors.grey, width: 1),
+              textMsg: 'نظر دو اینگونه میباشد',
+              onTap: () {
+                setState(() {
+                  controller.clear();
                   isActive1 = false;
+                  isActive2 = true;
                   isActive3 = false;
                   isActive4 = false;
-                },
-              ),
-              color: isActive2! ? Colors.blueAccent : Colors.white,
+                });
+              },
             ),
-            Card(
-              shape: const RoundedRectangleBorder(
-                side: BorderSide(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-              ),
-              child: ListTile(
-                title: Text(
-                  'نظر سه اینگونه میباشد',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyle(
-                    color: isActive3! ? Colors.black : Colors.black87,
-                  ),
-                ),
-                onTap: () {
-                  setState(() => isActive3 = !isActive3!);
+            QuestionMaker(
+              isActive: isActive3,
+              side: isActive3!
+                  ? const BorderSide(color: Colors.cyan, width: 1)
+                  : const BorderSide(color: Colors.grey, width: 1),
+              textMsg: 'نظر سه اینگونه میباشد',
+              onTap: () {
+                setState(() {
+                  controller.clear();
                   isActive1 = false;
                   isActive2 = false;
+                  isActive3 = true;
                   isActive4 = false;
-                },
-              ),
-              color: isActive3! ? Colors.blueAccent : Colors.white,
+                });
+              },
             ),
-            Card(
-              shape: const RoundedRectangleBorder(
-                side: BorderSide(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-              ),
-              child: ListTile(
-                title: Text(
-                  'نظر چهار اینگونه میباشد',
-                  textDirection: TextDirection.rtl,
-                  style: TextStyle(
-                    color: isActive4! ? Colors.black : Colors.black87,
-                  ),
-                ),
-                onTap: () {
-                  setState(() => isActive4 = !isActive4!);
+            QuestionMaker(
+              isActive: isActive4,
+              side: isActive4!
+                  ? const BorderSide(color: Colors.cyan, width: 1)
+                  : const BorderSide(color: Colors.grey, width: 1),
+              textMsg: 'نظر چهار اینگونه میباشد',
+              onTap: () {
+                setState(() {
+                  controller.clear();
                   isActive1 = false;
                   isActive2 = false;
                   isActive3 = false;
-                },
+                  isActive4 = true;
+                });
+              },
+            ),
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  borderSide: BorderSide(color: Colors.grey, width: 1),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  borderSide: BorderSide(color: Colors.cyan, width: 1),
+                ),
+                hintText: 'نظر شخصی خودتان را وارد کنید',
+                // suffix: MaterialButton(
+                //   child: const Text('ثبت'),
+                //   color: Colors.cyan,
+                //   shape: const RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.all(Radius.circular(25))),
+                //   onPressed: () {},
+                // ),
               ),
-              color: isActive4! ? Colors.blueAccent : Colors.white,
+              onChanged: (value) {
+                isActive1 = false;
+                isActive2 = false;
+                isActive3 = false;
+                isActive4 = false;
+                setState(() {});
+              },
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class QuestionMaker extends StatelessWidget {
+  const QuestionMaker({
+    Key? key,
+    this.isActive,
+    this.onTap,
+    this.textMsg,
+    this.side,
+  }) : super(key: key);
+  final bool? isActive;
+  final String? textMsg;
+  final BorderSide? side;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        side: side!,
+        borderRadius: const BorderRadius.all(Radius.circular(25)),
+      ),
+      child: ListTile(
+        title: Text(
+          textMsg!,
+          textDirection: TextDirection.rtl,
+          style: TextStyle(
+            color: isActive! ? Colors.black : Colors.black87,
+          ),
+        ),
+        onTap: onTap,
+      ),
+      color: isActive! ? Colors.cyan : Colors.white,
     );
   }
 }
--- lib/screens/question_screen.dart
+++ lib/screens/question_screen.dart
@@ -1,3 +1,6 @@
+import 'package:kar_zar/custom_widgets/question_holder_bubble.dart';
+import 'package:kar_zar/custom_widgets/appbar.dart';
+import 'package:kar_zar/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class QuestionScreen extends StatefulWidget {
@@ -9,8 +12,137 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  List? itemCount = [];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 100)
+              .copyWith(bottom: 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              GestureDetector(
+                child: const CustomAppBar(),
+                onTap: () {
+                  Navigator.popAndPushNamed(context, HomeScreen.id);
+                },
+              ),
+              const SizedBox(height: 5),
+              const Divider(color: Colors.grey),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 50, horizontal: 200)
+                        .copyWith(bottom: 24),
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(height: 50),
+                    const QuestionHolderBubble(),
+                    const SizedBox(height: 50),
+                    const Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: CustomTextField(
+                        hintText:
+                            'نام و نام خانوادگی خود را وارد کنید (اختیاری)',
+                        label: 'نام و نام خانوادگی',
+                        keyboardType: TextInputType.name,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const CustomTextField(
+                      hintText: 'شماره ی تلفن همراه خود وارد کنید (اجباری)',
+                      label: 'شماره ی همراه',
+                      keyboardType: TextInputType.phone,
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      ': رای دهید',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 34,
+                        color: Colors.cyan,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: 250,
+                      child: MaterialButton(
+                        color: Colors.cyan,
+                        height: 50,
+                        child: const Center(
+                          child: Text('ثبت'),
+                        ),
+                        onPressed: () {
+                          setState(() => itemCount!.add('user'));
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Text(
+                'رای دهندگان',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const Divider(color: Colors.grey),
+              SizedBox(
+                height: 500,
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 10,
+                    crossAxisSpacing: 40,
+                    mainAxisSpacing: 40,
+                  ),
+                  itemCount: itemCount!.length,
+                  itemBuilder: (context, index) {
+                    return const CircleAvatar(
+                      backgroundColor: Colors.lightBlueAccent,
+                      radius: 10,
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    Key? key,
+    this.hintText,
+    this.label,
+    this.keyboardType,
+  }) : super(key: key);
+  final String? hintText;
+  final String? label;
+  final TextInputType? keyboardType;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: keyboardType!,
+      decoration: InputDecoration(
+        hintText: hintText!,
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          borderSide: BorderSide(width: 1, color: Colors.grey),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          borderSide: BorderSide(width: 1, color: Colors.cyan),
+        ),
+        label: Text(label!),
+      ),
+    );
   }
 }
