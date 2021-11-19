4d0dc1f3c464e26fa69888b34cafc9e44ecbcd23
Sadra Galavani <sadra.galavani78@gmail.com>
2021-11-19 12:33:15 +0330

    question screen polished

--- lib/screens/question_screen.dart
+++ lib/screens/question_screen.dart
@@ -2,6 +2,7 @@ import 'package:kar_zar/custom_widgets/question_holder_bubble.dart';
 import 'package:kar_zar/custom_widgets/appbar.dart';
 import 'package:kar_zar/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({Key? key}) : super(key: key);
@@ -12,107 +13,175 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  Random random = Random();
   List? itemCount = [];
+  List? colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.amber,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple,
+  ];
+
+  List? alphabets = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+
+  Color getRandomColor() {
+    int color = random.nextInt(7);
+    return colors![color];
+  }
+
+  String getRandomAlphabets() {
+    int alphabet = random.nextInt(26);
+    return alphabets![alphabet];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 100)
-              .copyWith(bottom: 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              GestureDetector(
-                child: const CustomAppBar(),
-                onTap: () {
-                  Navigator.popAndPushNamed(context, HomeScreen.id);
-                },
-              ),
-              const SizedBox(height: 5),
-              const Divider(color: Colors.grey),
-              Padding(
+    double? isWeb = MediaQuery.of(context).size.width;
+    return isWeb <= 420
+        ? const Scaffold()
+        : Scaffold(
+            body: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Padding(
                 padding:
-                    const EdgeInsets.symmetric(vertical: 50, horizontal: 200)
-                        .copyWith(bottom: 24),
+                    const EdgeInsets.symmetric(vertical: 50, horizontal: 100)
+                        .copyWith(bottom: 0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
-                    const SizedBox(height: 50),
-                    const QuestionHolderBubble(),
-                    const SizedBox(height: 50),
-                    const Directionality(
-                      textDirection: TextDirection.rtl,
-                      child: CustomTextField(
-                        hintText:
-                            'نام و نام خانوادگی خود را وارد کنید (اختیاری)',
-                        label: 'نام و نام خانوادگی',
-                        keyboardType: TextInputType.name,
+                    GestureDetector(
+                      child: const CustomAppBar(),
+                      onTap: () {
+                        Navigator.popAndPushNamed(context, HomeScreen.id);
+                      },
+                    ),
+                    const SizedBox(height: 5),
+                    const Divider(color: Colors.grey),
+                    Padding(
+                      padding: isWeb > 420 && isWeb < 1200
+                          ? const EdgeInsets.symmetric(
+                                  vertical: 50, horizontal: 0)
+                              .copyWith(bottom: 0)
+                          : const EdgeInsets.symmetric(
+                                  vertical: 50, horizontal: 200)
+                              .copyWith(bottom: 0),
+                      child: Column(
+                        children: <Widget>[
+                          const SizedBox(height: 50),
+                          const QuestionHolderBubble(),
+                          const SizedBox(height: 50),
+                          const Directionality(
+                            textDirection: TextDirection.rtl,
+                            child: CustomTextField(
+                              hintText:
+                                  'نام و نام خانوادگی خود را وارد کنید (اختیاری)',
+                              label: 'نام و نام خانوادگی',
+                              keyboardType: TextInputType.name,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          const CustomTextField(
+                            hintText:
+                                'شماره ی تلفن همراه خود وارد کنید (اجباری)',
+                            label: 'شماره ی همراه',
+                            keyboardType: TextInputType.phone,
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            ': رای دهید',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 34,
+                              color: Colors.cyan,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          SizedBox(
+                            width: 250,
+                            child: MaterialButton(
+                              color: Colors.cyan,
+                              height: 50,
+                              child: const Center(
+                                child: Text('ثبت'),
+                              ),
+                              onPressed: () {
+                                setState(() => itemCount!.add('user'));
+                              },
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    const CustomTextField(
-                      hintText: 'شماره ی تلفن همراه خود وارد کنید (اجباری)',
-                      label: 'شماره ی همراه',
-                      keyboardType: TextInputType.phone,
-                    ),
-                    const SizedBox(height: 10),
                     const Text(
-                      ': رای دهید',
+                      'رای دهندگان',
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 34,
-                        color: Colors.cyan,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
-                    const SizedBox(height: 10),
+                    const Divider(color: Colors.grey),
                     SizedBox(
-                      width: 250,
-                      child: MaterialButton(
-                        color: Colors.cyan,
-                        height: 50,
-                        child: const Center(
-                          child: Text('ثبت'),
+                      height: 500,
+                      child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 10,
+                          crossAxisSpacing: 40,
+                          mainAxisSpacing: 40,
                         ),
-                        onPressed: () {
-                          setState(() => itemCount!.add('user'));
+                        itemCount: itemCount!.length,
+                        itemBuilder: (context, index) {
+                          return CircleAvatar(
+                            backgroundColor: getRandomColor(),
+                            radius: 10,
+                            child: Text(
+                              getRandomAlphabets(),
+                              style: const TextStyle(
+                                fontSize: 35,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                              ),
+                            ),
+                          );
                         },
                       ),
                     ),
+                    const SizedBox(height: 50),
                   ],
                 ),
               ),
-              const Text(
-                'رای دهندگان',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              const Divider(color: Colors.grey),
-              SizedBox(
-                height: 500,
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 10,
-                    crossAxisSpacing: 40,
-                    mainAxisSpacing: 40,
-                  ),
-                  itemCount: itemCount!.length,
-                  itemBuilder: (context, index) {
-                    return const CircleAvatar(
-                      backgroundColor: Colors.lightBlueAccent,
-                      radius: 10,
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 50),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
 
