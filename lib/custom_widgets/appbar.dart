91c0baecc1eb71f85021171f038d4fb907192a88
Sadra Galavani <sadra.galavani78@gmail.com>
2021-11-20 12:07:49 +0330

    admin header

--- lib/custom_widgets/appbar.dart
+++ lib/custom_widgets/appbar.dart
@@ -1,15 +1,20 @@
-import 'package:flutter/material.dart';
+import 'package:kar_zar/admin_screens/add_question_screen.dart';
+import 'package:kar_zar/admin_screens/questions_screen.dart';
+import 'package:kar_zar/admin_screens/results_screen.dart';
 import 'package:kar_zar/screens/home_screen.dart';
+import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({Key? key}) : super(key: key);
+  const CustomAppBar({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Column(
-        children: <Widget>[
-          Row(
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               Column(
@@ -40,16 +45,99 @@ class CustomAppBar extends StatelessWidget {
               ),
             ],
           ),
-          const SizedBox(height: 5),
-          const Divider(color: Colors.grey),
-        ],
-      ),
-      onTap: () {
-        Navigator.popAndPushNamed(
-          context,
-          HomeScreen.id,
-        );
-      },
+          onTap: () {
+            Navigator.popAndPushNamed(
+              context,
+              HomeScreen.id,
+            );
+          },
+        ),
+        const SizedBox(height: 5),
+        const Divider(color: Colors.grey),
+      ],
+    );
+  }
+}
+
+class CustomAdminAppBar extends StatelessWidget {
+  const CustomAdminAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                const SizedBox(width: 24),
+                TextButton(
+                  child: const Text('سوال ها'),
+                  onPressed: () {
+                    Navigator.popAndPushNamed(context, AdminQuestionsScreen.id);
+                  },
+                ),
+                const SizedBox(width: 24),
+                TextButton(
+                  child: const Text('افزودن سوال'),
+                  onPressed: () {
+                    Navigator.popAndPushNamed(
+                        context, AdminAddQuestionScreen.id);
+                  },
+                ),
+                const SizedBox(width: 24),
+                TextButton(
+                  child: const Text('گزارش'),
+                  onPressed: () {
+                    Navigator.popAndPushNamed(context, AdminResultsScreen.id);
+                  },
+                ),
+              ],
+            ),
+            GestureDetector(
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: const <Widget>[
+                      Text(
+                        'کارزار',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                        ),
+                      ),
+                      Text(
+                        'پلتفرم مستقل جمع اوری ارای شهرستان خوی',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 20),
+                  const CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(
+                      'https://media.istockphoto.com/photos/low-angle-of-tall-building-in-manhattan-picture-id1291177121?b=1&k=20&m=1291177121&s=170667a&w=0&h=aI1PkWS_GkXJ4Qz0gz2cTOud95SS4hUa4iZc1kqWYL4=',
+                    ),
+                  ),
+                ],
+              ),
+              onTap: () {
+                Navigator.popAndPushNamed(
+                  context,
+                  HomeScreen.id,
+                );
+              },
+            ),
+          ],
+        ),
+        const SizedBox(height: 5),
+        const Divider(color: Colors.grey),
+      ],
     );
   }
 }
