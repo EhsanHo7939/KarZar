a528dc72bd4f7746bc8dcf58dbb82b39cdbb7172
Sadra Galavani <sadra.galavani78@gmail.com>
2021-11-20 12:04:36 +0330

    admin add question screen

--- lib/admin_screens/add_question_screen.dart
+++ lib/admin_screens/add_question_screen.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:kar_zar/custom_widgets/appbar.dart';
 import 'package:kar_zar/custom_widgets/question_holder_bubble.dart';
+import 'package:kar_zar/custom_widgets/appbar.dart';
+import 'package:flutter/material.dart';
 
 class AdminAddQuestionScreen extends StatefulWidget {
   const AdminAddQuestionScreen({Key? key}) : super(key: key);
@@ -14,43 +14,73 @@ class _AdminAddQuestionScreenState extends State<AdminAddQuestionScreen> {
   @override
   Widget build(BuildContext context) {
     double? isWeb = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 100)
-              .copyWith(bottom: 0),
-          child: Column(
-            children: <Widget>[
-              const CustomAppBar(),
-              Padding(
-                padding: isWeb > 420 && isWeb < 1200
-                    ? const EdgeInsets.symmetric(vertical: 50, horizontal: 0)
-                        .copyWith(bottom: 0)
-                    : const EdgeInsets.symmetric(vertical: 50, horizontal: 200)
+    return isWeb <= 420
+        ? const Scaffold()
+        : Scaffold(
+            body: SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 50, horizontal: 100)
                         .copyWith(bottom: 0),
                 child: Column(
                   children: <Widget>[
-                    const AdminQuestionHolderBubble(),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: 250,
-                      child: MaterialButton(
-                        color: Colors.lightBlueAccent,
-                        height: 50,
-                        child: const Center(
-                          child: Text('ثبت سوال'),
-                        ),
-                        onPressed: () {},
+                    const CustomAdminAppBar(),
+                    Padding(
+                      padding: isWeb > 420 && isWeb < 1200
+                          ? const EdgeInsets.symmetric(
+                                  vertical: 50, horizontal: 0)
+                              .copyWith(bottom: 0)
+                          : const EdgeInsets.symmetric(
+                                  vertical: 50, horizontal: 200)
+                              .copyWith(bottom: 0),
+                      child: Column(
+                        children: <Widget>[
+                          const AdminQuestionHolderBubble(),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            width: 250,
+                            child: MaterialButton(
+                                color: Colors.lightBlueAccent,
+                                height: 50,
+                                child: const Center(
+                                  child: Text('ثبت سوال'),
+                                ),
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text(
+                                          'ارسال سوال',
+                                          textDirection: TextDirection.rtl,
+                                        ),
+                                        content: const Text(
+                                            'ایا از ارسال نظرسنجی مطمعن هستید'),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: const Text('خیر')),
+                                          TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: const Text('بله')),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                }),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
                       ),
-                    ),
-                    const SizedBox(height: 20),
+                    )
                   ],
                 ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+              ),
+            ),
+          );
   }
 }
