a9ee0a76947fcf5d01c43f1b237ecfe84174e760
Sadra Galavani <sadra.galavani78@gmail.com>
2021-11-20 10:40:22 +0330

    admin add question screen

--- lib/admin_screens/add_question_screen.dart
+++ lib/admin_screens/add_question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kar_zar/custom_widgets/appbar.dart';
+import 'package:kar_zar/custom_widgets/question_holder_bubble.dart';
 
 class AdminAddQuestionScreen extends StatefulWidget {
   const AdminAddQuestionScreen({Key? key}) : super(key: key);
@@ -11,6 +13,44 @@ class AdminAddQuestionScreen extends StatefulWidget {
 class _AdminAddQuestionScreenState extends State<AdminAddQuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double? isWeb = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 100)
+              .copyWith(bottom: 0),
+          child: Column(
+            children: <Widget>[
+              const CustomAppBar(),
+              Padding(
+                padding: isWeb > 420 && isWeb < 1200
+                    ? const EdgeInsets.symmetric(vertical: 50, horizontal: 0)
+                        .copyWith(bottom: 0)
+                    : const EdgeInsets.symmetric(vertical: 50, horizontal: 200)
+                        .copyWith(bottom: 0),
+                child: Column(
+                  children: <Widget>[
+                    const AdminQuestionHolderBubble(),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: 250,
+                      child: MaterialButton(
+                        color: Colors.lightBlueAccent,
+                        height: 50,
+                        child: const Center(
+                          child: Text('ثبت سوال'),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
