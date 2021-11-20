a4aca66565a0cc770351e1c05270c189000c357c
Sadra Galavani <sadra.galavani78@gmail.com>
2021-11-20 12:58:25 +0330

    result screen

--- lib/admin_screens/results_screen.dart
+++ lib/admin_screens/results_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kar_zar/custom_widgets/appbar.dart';
 
 class AdminResultsScreen extends StatefulWidget {
   const AdminResultsScreen({Key? key}) : super(key: key);
@@ -11,6 +12,16 @@ class AdminResultsScreen extends StatefulWidget {
 class _AdminResultsScreenState extends State<AdminResultsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 100)
+            .copyWith(bottom: 0),
+        child: Column(
+          children: const <Widget>[
+            CustomAdminAppBar(),
+          ],
+        ),
+      ),
+    );
   }
 }
