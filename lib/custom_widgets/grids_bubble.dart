da52fad97009dffd188d40916a3775fa4688c04e
Sadra Galavani <sadra.galavani78@gmail.com>
2021-12-03 20:19:13 +0330

    grids bubble changes

--- lib/custom_widgets/grids_bubble.dart
+++ lib/custom_widgets/grids_bubble.dart
@@ -1,143 +1,210 @@
-import 'package:kar_zar/networking/api.dart';
 import 'package:flutter/material.dart';
 
 class GridsBubble extends StatelessWidget {
   const GridsBubble({
     Key? key,
     this.color,
+    this.qBody,
   }) : super(key: key);
   final Color? color;
+  final String? qBody;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(24),
-      child: Container(
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: const BorderRadius.all(Radius.circular(8)).copyWith(
-            bottomRight: const Radius.circular(25),
-            bottomLeft: Radius.zero,
-          ),
-        ),
-        child: Stack(
-          alignment: Alignment.bottomRight,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+      padding: const EdgeInsets.all(25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(8),
+                topRight: Radius.circular(8),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                Expanded(
-                  flex: 5,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: color,
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(8)).copyWith(
-                        bottomRight: Radius.zero,
-                        bottomLeft: Radius.zero,
-                      ),
-                    ),
-                    child: const Center(
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 20, right: 5, left: 20),
-                        child: Text(
-                          ' ؟؟ \n '
-                          ' نفر شرکت کرده اند',
-                          textAlign: TextAlign.end,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'تعداد ۵۲۳۱ نفر'
+                    '\n'
+                    'شرکت کرده اند',
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
                 ),
-                Expanded(
-                  flex: 8,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 25),
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(25),
-                          bottomRight: Radius.circular(25),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          'https://static01.nyt.com/images/2020/07/17/business/00virus-cities1/00virus-cities1-mediumSquareAt3X.jpg',
                         ),
-                        color: Colors.white54,
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.only(top: 70, right: 20, left: 40),
-                        child: Text(
-                          ' روی سوال اینجا قرار خواهد گرفت',
-                          textAlign: TextAlign.end,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            color: Colors.black,
-                          ),
-                        ),
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
                 ),
               ],
             ),
-            Positioned(
-              right: 20,
-              bottom: 100,
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        'https://static01.nyt.com/images/2020/07/17/business/00virus-cities1/00virus-cities1-mediumSquareAt3X.jpg',
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(25)),
-                  ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text(
+              qBody!,
+              textAlign: TextAlign.end,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(color: Colors.white, child: const Divider()),
+          Container(
+            color: Colors.white,
+            child: const Directionality(
+              textDirection: TextDirection.rtl,
+              child: ListTile(
+                leading: Icon(Icons.person),
+                title: Text(
+                  'از طرف : من',
                 ),
               ),
             ),
-            Positioned(
-              right: 335,
-              bottom: 25,
-              child: FloatingActionButton(
-                backgroundColor: Colors.red,
-                child: const Icon(Icons.delete),
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        content: const Text('ایا از حذف سوال مطمعن هستید؟'),
-                        title: const Text('حدف؟'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: const Text('خیر'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Networking().deleteQ(context);
-                              Navigator.pop(context);
-                            },
-                            child: const Text('بله'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AdminGridsBubble extends StatelessWidget {
+  const AdminGridsBubble({
+    Key? key,
+    this.color,
+    this.qBody,
+    this.onPressed,
+  }) : super(key: key);
+  final Color? color;
+  final String? qBody;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(8),
+                topRight: Radius.circular(8),
               ),
-            )
-          ],
-        ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FloatingActionButton(
+                  backgroundColor: Colors.red,
+                  child: const Icon(Icons.delete),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          content: const Text('ایا از حذف سوال مطمعن هستید؟'),
+                          title: const Text('حدف؟'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text('خیر'),
+                            ),
+                            TextButton(
+                              onPressed: onPressed,
+                              child: const Text('بله'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'تعداد ۵۲۳۱ نفر'
+                    '\n'
+                    'شرکت کرده اند',
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          'https://static01.nyt.com/images/2020/07/17/business/00virus-cities1/00virus-cities1-mediumSquareAt3X.jpg',
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text(
+              qBody!,
+              textAlign: TextAlign.end,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(color: Colors.white, child: const Divider()),
+          Container(
+            color: Colors.white,
+            child: const Directionality(
+              textDirection: TextDirection.rtl,
+              child: ListTile(
+                leading: Icon(Icons.person),
+                title: Text(
+                  'از طرف : من',
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
