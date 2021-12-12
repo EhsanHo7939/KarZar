64965ad713dfa06e53ec3b64cce1e4c5837f0061
Ehsan Ho <EhsanHo7939@gmail.com>
2021-12-12 17:07:01 +0330

    CORS Config

--- KarZar/config/settings.py
+++ KarZar/config/settings.py
@@ -154,3 +154,24 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3200",
 ]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
