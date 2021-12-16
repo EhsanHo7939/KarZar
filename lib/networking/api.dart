f9cbdf56c78108c353e3eed0b55de1da85463a6e
Sadra Galavani <sadra.galavani78@gmail.com>
2021-12-16 20:52:18 +0330

    search api done

--- lib/networking/api.dart
+++ lib/networking/api.dart
@@ -66,21 +66,15 @@ class Networking {
     }
   }
 
-  Future<void> deleteQ(BuildContext context, int id) async {
+  Future<void> deleteQ(int id) async {
     url = 'http://127.0.0.1:8000/questions/$id/';
     Uri _uri = Uri.parse(url!);
     Response? response;
     try {
       response = await delete(_uri);
     } catch (e) {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          content: Center(
-            child: Text('something went wrong with error code:' + response!.statusCode.toString()),
-          ),
-        ),
-      );
+      print(e);
+      print(response!.statusCode);
     }
   }
 
@@ -97,7 +91,7 @@ class Networking {
     }
   }
 
-  Future<List<dynamic>> getVote(int id) async {
+  Future<List<dynamic>> getVote(int? id) async {
     url = 'http://127.0.0.1:8000/votes/?question=$id';
     Uri _uri = Uri.parse(url!);
     Response? response = await get(_uri);
@@ -128,6 +122,46 @@ class Networking {
     } catch (e) {
       print(e);
       print(response!.statusCode);
+    }
+  }
+
+  Future<void> countVote(String qBody, String option1, String option2, String option3, String option4, int option1count,
+      int option2count, int option3count, int option4count, int? id) async {
+    url = 'http://127.0.0.1:8000/questions/$id/';
+    Uri _uri = Uri.parse(url!);
+    Response? response;
+    try {
+      response = await put(
+        _uri,
+        body: {
+          "Q_Body": qBody,
+          "Option_1": option1,
+          "Option_2": option2,
+          "Option_3": option3,
+          "Option_4": option4,
+          "Option_1_count": "$option1count",
+          "Option_2_count": "$option2count",
+          "Option_3_count": "$option3count",
+          "Option_4_count": "$option4count",
+          "author": "1"
+        },
+      );
+    } catch (e) {
+      print(e);
+      print(response!.statusCode);
+    }
+  }
+
+  Future<List<dynamic>> searchQ(String value) async {
+    url = "http://127.0.0.1:8000/questions/?search=$value";
+    Uri _uri = Uri.parse(url!);
+    Response? response = await get(_uri);
+
+    if (response.statusCode == 200) {
+      var data = json.decode(utf8.decode(response.bodyBytes));
+      return data;
+    } else {
+      return [];
     }
   }
 }
